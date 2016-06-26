1e9d16abd55bfb14af34898883dec959f9ca8597
Technogeek6952 <tg6952@gmail.com>
2016-06-25 18:34:43 -0600

    Stable Release v1.0.3_a01
    First stable release - plan is to start branching and merging for new features

--- src/com/julianEngine/Engine2D.java
+++ src/com/julianEngine/Engine2D.java
@@ -61,7 +61,7 @@ import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
  */
 public class Engine2D extends JFrame implements WindowListener, KeyListener {
 	/*--------Public Static Variables-------*/
-	public static String versionID = "v1.2.4_a03"; //Engine version
+	public static String versionID = "v1.2.5_a05"; //Engine version
 	public static JDFMaster masterFile; //Variable holder for the master plugin file (see JDFMaster)
 	public static ArrayList<JDFPlugin> pluginFiles; //ArrayList holder for each plugin file (see JDFPlugin)
 	public static boolean debugMode = true; //Should the engine be run in debug mode (set to false for release)
