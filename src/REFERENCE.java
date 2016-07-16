4883cc14862accc196b76a4b5f0a68944dc3ef5f
Technogeek6952 <tg6952@gmail.com>
2016-07-16 13:32:39 -0600

    fixed some countries
    -more progress to be put on another feature branch

--- src/REFERENCE.java
+++ src/REFERENCE.java
@@ -16,4 +16,8 @@ public class REFERENCE {
 	//Sometimes when launching a rare error (probably related to threads and not checking locks, or something) 
 	//causes the engine to not be set up correctly, and nothing will happen except an exception for buffers not being created
 	//unfortunately I cannot seem to replicate this error reliably. It also appears to be very rare.
+	
+	//TODO: some sort of world history system, so that the user can press a button (esc?) to go to the last screen
+	//add an interface to the world.java file that can be added as a listener to any world. Would report keys pressed if on that
+	//screen. if the key is esc (or other), go back in history, or possibly load pause menu
 }
